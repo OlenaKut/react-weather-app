b91e6d00e713c7e864b202be7f3022c00ccde480
OlenaKut <cundrochka@gmail.com>
2022-09-09 19:57:58 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,24 +4,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a href="https://www.linkedin.com/in/%D0%BE%D0%BB%D0%B5%D0%BD%D0%B0-%D0%BA%D1%83%D1%82%D0%B0%D1%81%D0%B5%D0%B2%D0%B8%D1%87-b0b99a24b/" target="_blank"
-          rel="noreferrer">
-          {" "}
-          Olena Kutasevych
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/OlenaKut/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-soursed on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/%D0%BE%D0%BB%D0%B5%D0%BD%D0%B0-%D0%BA%D1%83%D1%82%D0%B0%D1%81%D0%B5%D0%B2%D0%B8%D1%87-b0b99a24b/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Olena Kutasevych
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/OlenaKut/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-soursed on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
